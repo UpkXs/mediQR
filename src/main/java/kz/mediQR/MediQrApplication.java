341cd4da9bc732a970579327df736128b794488d
asugurali <asugurali@greet-go.com>
2023-06-02 17:29:52 +0600

    feat(): initial

---
+++ src/main/java/kz/mediQR/MediQrApplication.java
@@ -0,0 +1,13 @@
+package kz.mediQR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediQrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediQrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/mediQR/MediQrApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.mediQR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
