0828cb05867ae11f36c3c67edd8b2b96f4986594
asugurali <asugurali@greet-go.com>
2023-06-02 17:30:12 +0600

    feat(): create Reason model

---
+++ src/main/java/kz/mediQR/model/Reason.java
@@ -0,0 +1,13 @@
+package kz.mediQR.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reason {
+    private String id;
+    private String name;
+}
