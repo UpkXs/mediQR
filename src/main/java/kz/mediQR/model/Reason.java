e6f05ff6bdb0306eba8a4e14e0cc6860079951c4
Arman <aaj.aro@mail.ru>
2023-06-04 01:39:45 +0600

    fix(): changed Reason model to JPA(to create table)

--- src/main/java/kz/mediQR/model/Reason.java
+++ src/main/java/kz/mediQR/model/Reason.java
@@ -1,13 +1,21 @@
 package kz.mediQR.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "reason")
 public class Reason {
+    @Id
     private String id;
+
+    @NotBlank(message = "Name is required")
     private String name;
 }
